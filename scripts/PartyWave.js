39d2a81c61ef66588123cef00307973a1c9ee044
amysimmons <amy__simmons@hotmail.com>
2016-02-28 13:00:57 +1100

    first commit

---
+++ scripts/PartyWave.js
@@ -0,0 +1,12 @@
+const React = require('react');
+
+const PartyWave = React.createClass({
+  render(){
+    return(
+      <div>Hello Party Wave!</div>
+    );
+  }
+});
+
+
+module.exports = PartyWave;
---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+const PartyWave = require('./PartyWave');
+
+ReactDom.render(<PartyWave />, document.querySelector('#container'));
