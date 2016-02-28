c9abc65f0534113be949695196f6ba2149e6db2b
amysimmons <amy__simmons@hotmail.com>
2016-02-29 01:30:38 +1100

    toggling selected class on nav links

--- scripts/Header.js
+++ scripts/Header.js
@@ -3,12 +3,29 @@ require("../styles/Header.css");
 
 const Header = React.createClass({
   render(){
+
+    let spotsClasses;
+
+    if (this.props.route === "/spots") {
+      spotsClasses="spots-navlink selected";
+    }else {
+      spotsClasses="spots-navlink";
+    }
+
+    let aboutClasses;
+
+    if (this.props.route === "/about") {
+      aboutClasses="about-navlink selected";
+    }else {
+      aboutClasses="about-navlink";
+    }
+
     return (
       <header>
       	<nav>
       		<div className="navlinksleft">
-				<a href="#/spots" className="spots-navlink">Spots</a>
-				<a href="#/about" className="about-navlink">About</a>
+				<a href="#/spots" className={spotsClasses}>Spots</a>
+				<a href="#/about" className={aboutClasses}>About</a>
 			</div>
 			<div className="navlinksright">
 				<a href="#/"><img src='../images/PW_logo.svg'></img></a>
--- scripts/PartyWave.js
+++ scripts/PartyWave.js
@@ -21,12 +21,6 @@ const PartyWave = React.createClass({
   },
 
   render() {
-    switch (this.state.route) {
-      case '/': break;
-      case '/spots': break;
-      case '/about': break;
-    }
-
     let Child
     switch (this.state.route) {
       case '/spots': Child = HeroImage; break;
@@ -34,9 +28,12 @@ const PartyWave = React.createClass({
       default:      Child = HeroImage;
     }
 
+    let route = this.state.route;
+
 	return(
 		<div>
-			<Header/>
+			<Header
+				route={route}/>
 			<div>
 				<Child/>
 			</div>
@@ -47,54 +44,3 @@ const PartyWave = React.createClass({
 });
 
 module.exports = PartyWave;
-
-
-/*
-var App = React.createClass({
-  getInitialState(){
-    let	storyData = this.loadData();
-    let selectedStory = null;
-    let selectedStories = 0;
-
-    return{
-      storyData: storyData,
-      selectedStory: selectedStory,
-      selectedStories: selectedStories
-    };
-  },
-  loadData(){
-  	return require("json!../Data/stories.json");
-  },
-  selectStory(selectedStory){
-  	var selectedStory = selectedStory;
-    var selectedStories = this.state.selectedStories;
-
-    if (!selectedStory["selected"]) {
-      selectedStories++;
-      selectedStory["selected"] = true;
-    }
-    this.setState({selectedStory:selectedStory, selectedStories:selectedStories});
-  },
-  render(){
-  	var storyData = this.state.storyData;
-  	var selectedStory = this.state.selectedStory;
-  	var selectedStories = this.state.selectedStories;
-  	var selectStory = this.selectStory;
-
-    return (
-      <div className="app-container">
-      	<Header/>
-        <Findings
-          storyData={storyData}/>
-        <Recommendations/>
-      	<StorySelectors 
-      		storyData={storyData}
-      		selectedStory={selectedStory}
-      		selectedStories={selectedStories}
-      		selectStory={selectStory}/>
-      	<Footer/>
-      </div>
-    )
-  }
-});
-*/
